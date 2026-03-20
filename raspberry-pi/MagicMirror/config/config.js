b58c9fa3e56df1af6cca8f5b4b8285cdbf7dbaef
Faber767 <achim-leissner@gmx.de>
2026-03-20 12:49:16 +0100

    MMM_MotionDetector Test2

--- raspberry-pi/MagicMirror/config/config.js
+++ raspberry-pi/MagicMirror/config/config.js
@@ -9,7 +9,7 @@
  * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
  */
 let config = {
-	address: "localhost",	// Address to listen on, can be:
+	address: "0.0.0.0",	// Address to listen on, can be:
 							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 							// - another specific IPv4/6 to listen on a specific interface
 							// - "0.0.0.0", "::" to listen on any interface
@@ -17,7 +17,7 @@ let config = {
 	port: 8080,
 	basePath: "/",	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
 									// you must set the sub path here. basePath must end with a /
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],	// Set [] to allow all IP addresses
+	ipWhitelist: [],	// Set [] to allow all IP addresses
 									// or add a specific IPv4 of 192.168.1.5 :
 									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -49,20 +49,20 @@ let config = {
 			module: "clock",
 			position: "top_left"
 		},
-		{
-			module: "calendar",
-			header: "US Holidays",
-			position: "top_left",
-			config: {
-				calendars: [
-					{
-						fetchInterval: 7 * 24 * 60 * 60 * 1000,
-						symbol: "calendar-check",
-						url: "https://ics.calendarlabs.com/76/mm3137/US_Holidays.ics"
-					}
-				]
-			}
-		},
+//		{
+//			module: "calendar",
+//			header: "US Holidays",
+//			position: "top_left",
+//			config: {
+//				calendars: [
+//					{
+//						fetchInterval: 7 * 24 * 60 * 60 * 1000,
+//						symbol: "calendar-check",
+//						url: "https://ics.calendarlabs.com/76/mm3137/US_Holidays.ics"
+//					}
+//				]
+//			}
+//		},
 		{
 			module: "compliments",
 			position: "lower_third"
@@ -83,18 +83,74 @@ let config = {
 				broadcastNewsUpdates: true
 			}
 		},
-		{
+    		{
 			module: "weather",
 			position: "top_right",
 			header: "Weather Forecast",
 			config: {
 				weatherProvider: "openmeteo",
+				lang: "de",
+				units: "metric",
 				type: "forecast",
-				lat: 40.776676,
-				lon: -73.971321
+				lat: 49.44,
+				lon: 11.88
 			}
 		},
-    		
+	
+{
+  module: "MMM-JsonFile",
+  position: "top_left",
+  config: {
+    filePath: "/home/fsmt2-frontend/MagicMirror/modules/MMM-JsonFile/Sensordaten.Json",
+    updateInterval: 2000,
+    title: "Sensorwerte",
+    sortKeys: true,
+    showNested: true
+  }
+},
+{
+    module: 'MMM-Remote-Control',
+    position: 'bottom_left', // Required to show URL/QR code on mirror
+    // you can hide this module afterwards from the remote control itself
+    config: {
+        customCommand: {},  // Optional, See "Using Custom Commands" below
+        showModuleApiMenu: true, // Optional, Enable the Module Controls menu
+        secureEndpoints: true, // Optional, See API/README.md
+        // uncomment any of the lines below if you're gonna use it
+        // customMenu: "custom_menu.json", // Optional, See "Custom Menu Items" below
+        // apiKey: "", // Optional, See API/README.md for details
+        // classes: {}, // Optional, See "Custom Classes" below
+
+        // QR Code options (new!)
+         showQRCode: true, // Optional, display QR code for easy mobile access (default: true)
+         qrCodeSize: 150, // Optional, size of QR code in pixels (default: 150)
+         qrCodePosition: "replace" // Optional:
+        //   "below" - Show URL above, QR code below (default)
+        //   "above" - Show QR code above, URL below
+        //   "replace" - Show only QR code, no URL text
+    }
+},
+			{
+		  module: "MMM-MotionDetector",
+		  position: "top_left",
+		  config: {
+		    device: "/dev/video0",
+		    captureIntervalTime: 1000,
+		    threshold: 1500,
+		    scoreThreshold: 20,
+		    timeout: 10000,
+		    turnOffDisplay: true,
+		    turnOnDisplay: true,
+		    displayCmd: {
+		      "on": "xset dpms force on",
+		      "off": "xset dpms force off"
+		    }
+		        //displayCommandOff: "xset dpms force off",   // für X11
+        		//displayCommandOn: "xset dpms force on",     // für X11
+			//keepDisplayOff: true
+		  }
+		},
+
 	]
 };
 
